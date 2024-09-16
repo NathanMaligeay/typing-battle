56f6b5e142f910c65cf271711443c375da6a20d1
NathanMaligeay <nathan_maligeay@hotmail.fr>
2024-09-16 20:59:22 +0200

    started working on backend implementationwith express and psql

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 
 // Create a new pool instance with your PostgreSQL connection details
 const pool = new Pool({
-    user: 'nathan', // replace with your PostgreSQL username
+    user: 'postgres', // replace with your PostgreSQL username
     host: 'localhost',
     database: 'typing_battle', // replace with your PostgreSQL database name
-    password: 'lullaby180100', // replace with your PostgreSQL password
+    password: 'postgres', // replace with your PostgreSQL password
     port: 5432, // default port for PostgreSQL
 });
 
