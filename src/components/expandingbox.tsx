2eb9f05ef6a54c96d058f217af2a146dbb5595c0
NathanMaligeay <nathan.maligeay@hotmail.fr>
2024-10-02 21:39:18 +0200

    more work

--- src/components/expandingbox.tsx
+++ src/components/expandingbox.tsx
@@ -11,6 +11,7 @@ const ExpandingBox: React.FC<ExpandingBoxProps> = ({username, handleLogout}) => 
   const [gameCount, setGameCount] = useState<number | null>(null);
   const [totalWordsTyped, setTotalWordsTyped] = useState<number | null>(0);
   const [meanWordsTyped, setMeanWordsTyped] = useState<number | null>(0);
+  const [accuracy, setAccuracy] = useState<number | null>(null);
 
 
 
@@ -22,12 +23,11 @@ const ExpandingBox: React.FC<ExpandingBoxProps> = ({username, handleLogout}) => 
     const fetchGameCount = async () => {
         try {
             const data = await getNumberGames(username);
-            setGameCount(data.count);
-            console.log(data.count)
-            setTotalWordsTyped(data.total_words_typed);
-            console.log(data.total_words_typed)
-            setMeanWordsTyped(data.mean_words_typed);
-            console.log(data.mean_words_typed)
+            const {count, total_words_typed, mean_words_typed, mean_accuracy} = data;
+            setGameCount(count);
+            setTotalWordsTyped(total_words_typed);
+            setMeanWordsTyped(mean_words_typed);
+            setAccuracy(mean_accuracy)
         } catch (error) {
             if (error instanceof Error) alert(`Error: ${error.message}`);
         }
@@ -43,10 +43,11 @@ const ExpandingBox: React.FC<ExpandingBoxProps> = ({username, handleLogout}) => 
       className={`box ${isExpanded ? "expanded" : ""}`}
       onClick={toggleExpand}
     >
-      {isExpanded ? <div>
-        <div>Number of games played : {gameCount}</div>
-        <div>Total number of words typed : {totalWordsTyped}</div>
-        <div>Avg. number of words typed : {meanWordsTyped}</div>
+      {isExpanded ? <div className="expandedbox">
+        <div>Games played : {gameCount}</div>
+        <div>Words typed : {totalWordsTyped}</div>
+        <div>Avg. words typed : {meanWordsTyped}</div>
+        <div>Avg. accuracy: {accuracy}</div>
         <div>
           <button onClick={handleLogout} className="logoutButton">Logout</button>
           <button className="logoutButton">View games</button>
