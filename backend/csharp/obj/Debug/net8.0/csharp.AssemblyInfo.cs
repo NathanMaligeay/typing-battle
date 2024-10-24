b5437beff9e10761cff2b7871b83301d31ce8738
NathanMaligeay <nathan.maligeay@hotmail.fr>
2024-10-24 14:40:16 +0200

    c

--- backend/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ backend/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c0740d950328c4e1edc8c51ba7265540efb29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a4d990fda8c8994e25be685f87210614ecbcc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
