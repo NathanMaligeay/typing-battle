26a30aa82c18c45bef929cb59a5f3e79f61bd1fe
NathanMaligeay <nathan.maligeay@hotmail.fr>
2024-10-27 23:12:21 +0100

    feat: added swagger back + changed new password in connection string

--- backend/csharp/Program.cs
+++ backend/csharp/Program.cs
@@ -48,8 +48,8 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-//app.UseSwagger();
-//app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
