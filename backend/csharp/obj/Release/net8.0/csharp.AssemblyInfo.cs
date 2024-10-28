b3dd0aeaac6e161b56e2cd28855cc951180fbe48
NathanMaligeay <nathan.maligeay@hotmail.fr>
2024-10-28 16:46:55 +0100

    bleeeh

--- backend/csharp/obj/Release/net8.0/csharp.AssemblyInfo.cs
+++ backend/csharp/obj/Release/net8.0/csharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb503a7f1d53d33975138b5a89277e60033adbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f497f3babc5dbcee53004554b87ddca781c89fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
